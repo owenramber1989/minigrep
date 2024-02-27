f8f14ea008619d843c0df29e9e6693bfff8b6b41
owenramber1989 <owenramber1989@163.com>
2024-02-27 09:13:21 +0800

    <optimize>: <simplified code with iterators>

--- src/lib.rs
+++ src/lib.rs
@@ -45,24 +45,18 @@ Trust me.";
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 pub struct Config {
     pub query: String,
@@ -71,16 +65,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("missing arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        let ignore_case = env::var("IGNORE_CASE").map_or(
-            args.iter().any(|arg| arg.to_lowercase().contains("-i")),
-            |env_value| env_value.eq("1"),
-        );
+    pub fn new(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Missing query string"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Missing file path"),
+        };
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
         Ok(Config {
             query,
             file_path,
